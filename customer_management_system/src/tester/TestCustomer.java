b930e78df36b594aa2f98f6d33080c750381bd07
Gopalsangwe <gopalsangwe15@gmail.com>
2023-11-07 17:36:00 +0530

    update

--- customer_management_system/src/tester/TestCustomer.java
+++ customer_management_system/src/tester/TestCustomer.java
@@ -1,10 +1,12 @@
 package tester;
 
-import java.util.List;
 import java.util.Scanner;
 
-import static utils.CustomerUtils.*;
+
 import com.code.Customer;
+import com.code.ServicePlan;
+
+import static utils.customerValidation.validateAllInputs;
 
 public class TestCustomer {
 	
@@ -14,44 +16,14 @@ public class TestCustomer {
 		
          try(Scanner sc=new Scanner(System.in)){
         	 
-        	 List<Customer> Display=CustomerRecord();
+        	 System.out.println("Enter Customer Details : firstname ,lastname ,email , password,registrationAmount,dob,Service plan");
+        	 Customer cust = validateAllInputs(sc.next(),sc.next(),sc.next(),sc.next(),sc.nextDouble(),sc.next(),sc.next());
+        	 System.out.println(cust);
         	 
-        	 boolean exit=false;
-        	 
-        	 while(!exit) {
-        		 
-        		 System.out.println("Options : 1.Add Customer \n" + "2.Show All Customers \n" + "3.Exit \n");
-        		 
-        	      switch(sc.nextInt()) {
-        	      
-        	      case 1:
-        	    	  System.out.println("Add Customer");
-        	    	  break;
-        	      case 2:
-        	    	  System.out.println("Showing All Customers");
-        	    	  for(Customer c: Display) {
-        	    		  System.out.println(c);
-        	    	  }
-        	    	  break;
-        	      case 3:
-        	    	  break;
-        	      }	 
-        	 
-        	 }
-        	 
-         	
-//        	 System.out.println("Enter Customer Details : firstname ,lastname ,email , password,registrationAmount,dob,Service plan");
-//        	 Customer cust = validateAllInputs(sc.next(),sc.next(),sc.next(),sc.next(),sc.nextDouble(),sc.next(),sc.next());
-//        	 System.out.println(cust);
-//        	 
-//        	 System.out.println("Enter Customer Details : firstname ,lastname ,email , password,registrationAmount,dob,Service plan");
-//        	 Customer cust2 = validateAllInputs(sc.next(),sc.next(),sc.next(),sc.next(),sc.nextDouble(),sc.next(),sc.next());
-//        	 System.out.println(cust2);
+        	 System.out.println("Enter Customer Details : firstname ,lastname ,email , password,registrationAmount,dob,Service plan");
+        	 Customer cust2 = validateAllInputs(sc.next(),sc.next(),sc.next(),sc.next(),sc.nextDouble(),sc.next(),sc.next());
+        	 System.out.println(cust2);
          }
-         
-         
-         
-         
          catch (Exception e) {
         	 System.out.println(e);
         	 e.printStackTrace();
